74fe48f0acc2684e7171d2e9f67cc3088cf5c5c8
Sep Bar <sepand.barkhodaee@gmail.com>
2014-02-07 19:52:39 -0500

    Fixed bugs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,9 +7,8 @@ class ProductsController < ApplicationController
    end
 
    def show
-  	products_json = open('http://lcboapi.com/products').read
-  	@products = JSON.parse(products_json)
-  	@product = @products['result'].select {|p| p['id'].to_i == params[:id].to_i}.first
+  	products_json = open("http://lcboapi.com/products/#{params[:id]}").read
+  	@product = JSON.parse(products_json)
 
    end
 
